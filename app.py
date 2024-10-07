8389f5a9a9fd98e74dcca7f97df2300525f12d89
Nicobpm <brayan-puentes@hotmail.com>
2024-10-07 23:47:39 +0200

    .

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ data_VH = pd.read_csv("vehicles_us.csv")
 
 data_VH['model_year'] = data_VH['model_year'].fillna(0).astype(int)
 
-st.header('You can select the range of years to analyze')
+st.header('We can see data about vehicles sales')
+st.write("Please select the year of the car model to analize"
+         )
 min_year = 1908
 max_year = 2019
 
@@ -15,14 +17,21 @@ selected_year = st.slider('Select year:',
                           max_value=max_year,
                           value=max_year,
                           step=1)
-if selected_year:
+filtered_year = data_VH[(data_VH['year'] >= selected_year[0]) & (
+    data_VH['year'] <= selected_year[1])]
+
+if not filtered_year.empty:
     st.write('Range of years to show:', selected_year)
-    fig = px.scatter(data_VH, x="odometer", y="price")
+    fig = px.scatter(filtered_year, x="odometer", y="price",
+                     title=filtered_year, x="odometer", y="price",
+                     title=f'Scatter plot of Price vs Odometer for years 
+                     {selected_year[0]} to {selected_year[1]}')
     st.plotly_chart(fig, use_container_width=True)
+else:
+    st.write('No data available for the selected year range.')
 
-st.header('Build a dispersion graph')
+st.header('Build a histogram for media odometer')
 Button = st.button('Click to build graph')
-
 
 if Button:
     st.write('Make a new graph displaying the price in milles')
