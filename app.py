42be5a424d175a3bc7956035708644a47ac89b09
Nicobpm <brayan-puentes@hotmail.com>
2024-10-16 01:51:09 -0700

    improve graphs

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
-
+# Read the file
 data_VH = pd.read_csv("vehicles_us.csv")
-
+# Fill in the empty model_years and convert to Number column
 data_VH['model_year'] = data_VH['model_year'].fillna(0).astype(int)
 
+# Create a header about the process
 st.header('We can see data about vehicles sales')
 st.write("Please select the year of the car model to analize"
          )
 min_year = 1908
 max_year = 2019
 
-selected_year = st.slider('Select year:',
+# Make a slider to control for customers between model years
+selected_year = st.slider('Select year range:',
                           min_value=min_year,
                           max_value=max_year,
-                          value=max_year,
+                          value=(min_year, max_year),
                           step=1)
-filtered_year = data_VH[(data_VH['year'] >= selected_year[0]) & (
-    data_VH['year'] <= selected_year[1])]
+filtered_year = data_VH[(data_VH['model_year'] >= selected_year[0]) &
+                        (data_VH['model_year'] <= selected_year[1])]
 
+# Crate a scatter using the filter by slider
 if not filtered_year.empty:
     st.write('Range of years to show:', selected_year)
-    title = f'Scatter plot of Price vs Odometer for years {
+    title = f'Scatter graph of Price vs milles for years {
         selected_year[0]} to {selected_year[1]}'
     fig = px.scatter(
         filtered_year,
@@ -35,11 +38,11 @@ if not filtered_year.empty:
 else:
     st.write('No data available for the selected year range.')
 
+# Create a graph using button for histogram
 st.header('Build a histogram for media odometer')
 Button = st.button('Click to build graph')
 
 if Button:
-    st.write('Make a new graph displaying the price in milles')
+    st.write('displaying the price in milles')
     fig = px.histogram(data_VH, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
-
